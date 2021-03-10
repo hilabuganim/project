ba994739f224d2e3bc9085ba2f3b3cb884832a7c
hilab <hilab@10.0.0.16>
2021-03-10 18:21:29 +0200

    Targil 1

---
+++ src/primitives/Point3D.java
@@ -0,0 +1,103 @@
+package primitives;
+
+public class Point3D {
+ Coordinate x;
+ Coordinate y;
+ Coordinate z;
+ /**
+  * Defining the zero-beginning points of the axes
+  */
+	public static Point3D ZERO = new Point3D(0, 0, 0);
+/**
+ * 
+ * @param vector
+ * @return Connection of two vectors
+ */
+	public Point3D add(Vector vector) {
+		return new Point3D(this.x.coord + vector.head.x.coord, this.y.coord + vector.head.y.coord, this.z.coord + vector.head.z.coord);
+	}
+/**
+ * constructor of Coordinate
+ * @param x
+ * @param y
+ * @param z
+ */
+	public Point3D(Coordinate x, Coordinate y, Coordinate z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+/**
+ * constructor of double
+ * @param x
+ * @param y
+ * @param z
+ */
+	public Point3D(double x, double y, double z) {
+		this.x = new Coordinate(x);
+		this.y = new Coordinate(y);
+		this.z = new Coordinate(z);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point3D other = (Point3D) obj;
+		if (x == null) {
+			if (other.x != null)
+				return false;
+		} else if (!x.equals(other.x))
+			return false;
+		if (y == null) {
+			if (other.y != null)
+				return false;
+		} else if (!y.equals(other.y))
+			return false;
+		if (z == null) {
+			if (other.z != null)
+				return false;
+		} else if (!z.equals(other.z))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + this.x + ", " + this.y + ", " + this.z + ")";
+	}
+/**
+ * 
+ * @param point
+ * @return A function that gets a point and calculates the subtraction between two vectors
+ */
+	public Vector subtract(Point3D point) {
+		return new Vector(this.x.coord - point.x.coord, this.y.coord - point.y.coord, this.z.coord - point.z.coord);
+	}
+/**
+ * A function that gets a point and calculates the distance squared between them
+ * @param point
+ * @return distance Squared
+ */
+public double distanceSquarde(Point3D point)
+{
+	double a=(this.x.coord - point.x.coord)*(this.x.coord - point.x.coord);
+	double b=(this.y.coord - point.y.coord)*(this.y.coord - point.y.coord);
+	double c=(this.z.coord - point.z.coord)*(this.z.coord - point.z.coord);
+	return a+b+c;
+}
+/**
+ * A function that calculates the distance with the function sqrt
+ * on the previous function with the given point
+ * @param point
+ * @return distance
+ */
+public double distance(Point3D point)
+{
+	return Math.sqrt(distanceSquarde(point));
+}
+}
---
+++ src/primitives/Vector.java
@@ -0,0 +1,150 @@
+package primitives;
+
+public class Vector {
+	Point3D head;
+/**
+ * Checks if the given vector is a zero vector and 
+ * if so throws an exception if not set it
+ * @param x
+ * @param y
+ * @param z
+ */
+	public Vector(Coordinate x, Coordinate y, Coordinate z) {
+		Point3D point = new Point3D(x, y, z);
+		if (point.equals(Point3D.ZERO))
+			throw new IllegalArgumentException("zero vector");
+		this.head = point;
+	}
+/**
+ * Checks if the given vector is a zero vector and 
+ * if so throws an exception if not set it
+ * @param x
+ * @param y
+ * @param z
+ */
+	public Vector(double x, double y, double z) {
+		Point3D point = new Point3D(x, y, z);
+		if (point.equals(Point3D.ZERO))
+			throw new IllegalArgumentException("zero vector");
+		this.head = new Point3D(x, y, z);
+	}
+
+	public Vector(Point3D point) {
+		this.head = point;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector other = (Vector) obj;
+		if (head == null) {
+			if (other.head != null)
+				return false;
+		} else if (!head.equals(other.head))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return head.toString();
+	}
+/**
+ * Gets a vector normalized by a formula
+ * @return Normalized vector
+ */
+	public Vector normalize() {
+		this.head = scale(1 / length()).head;
+		return this;
+	}
+/**
+ * A function that use the previous function to normalize the vector
+ * with the same direction
+ * @return new vector
+ */
+	public Vector normalized() {
+		return new Vector(head).normalize();
+	}
+
+	/**
+	 * A function that add one vector to the other
+	 * @param vector
+	 * @return new vector
+	 */
+	public Vector add(Vector vector) {
+		return new Vector(head.add(vector));
+	}
+/**
+ * A function that subtract one vector from the other
+ * @param vector
+ * @return new vector
+ */
+	public Vector subtract(Vector vector) {
+		return this.head.subtract(vector.head);
+	}
+/**
+ * A function that doubles the vector in the scale
+ * @param s
+ * @return Vector after multiplication
+ */
+	public Vector scale(double s) {
+		return new Vector(head.x.coord * s, head.y.coord * s, head.z.coord * s);
+	}
+/**
+ * A function that makes a vector product by a formula
+ * @param vector
+ * @return new vector
+ */
+	public Vector crossProduct(Vector vector) {
+		double Ax = this.head.x.coord;
+		double Ay = this.head.y.coord;
+		double Az = this.head.z.coord;
+
+		double Bx = vector.head.x.coord;
+		double By = vector.head.y.coord;
+		double Bz = vector.head.z.coord;
+
+		return new Vector(Ay * Bz - Az * By, Az * Bx - Ax * Bz, Ax * By - Ay * Bx);
+	}
+/**
+ * A function that makes a scalar product of vectors
+ * @param vector
+ * @return new vector
+ */
+	public double dotProduct(Vector vector) {
+		double Ax = this.head.x.coord;
+		double Ay = this.head.y.coord;
+		double Az = this.head.z.coord;
+
+		double Bx = vector.head.x.coord;
+		double By = vector.head.y.coord;
+		double Bz = vector.head.z.coord;
+
+		return (Ax * Bx + Ay * By + Az * Bz);
+	}
+/**
+ * A function that calculate the vector length squared
+ * @return double
+ */
+	public double lengthSquared() {
+		return this.head.distanceSquarde(Point3D.ZERO);
+	}
+/**
+ * A function that use the previous function to Calculate the vector length
+ * by the function sqrt
+ * @return double
+ */
+	public double length() {
+		return Math.sqrt(lengthSquared());
+	}
+
+	public Point3D getHead() {
+		return this.head;
+	}
+
+}
